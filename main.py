0400a3973732b729aa7697f1113c8d5624e274a3
slepeylukee08 <159123907+slepeylukee08@users.noreply.github.com>
2025-01-21 11:51:35 +0100

    add_row

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
-# Dvourozměrné pole s 3 řádky a 3 sloupci
+# Původní dvourozměrné pole s 3 řádky a 3 sloupci
 matrix = [
     [1, 2, 3],
     [4, 5, 6],
     [7, 8, 9]
 ]
 
-# Vytisknutí pole
+# Přidání nového řádku
+matrix.append([10, 11, 12])
+
+# Přidání nového sloupce (přidáme hodnotu k jednotlivým řádkům)
+for row in matrix:
+    row.append(0)  # Přidání hodnoty 0 jako nový sloupec
+
+# Vytisknutí upraveného pole
 for row in matrix:
     print(row)
